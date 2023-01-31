399f1e48731e1d245b255613460cdc4f4d451c27
Arshia Nazarlotfi <arshia_nazarlotfi1376@yahoo.com>
2023-01-31 23:54:16 +0330

    adding headers

--- src/models/auth/auth.controller.ts
+++ src/models/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, HttpCode } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, Header } from '@nestjs/common';
 import { AuthServiceV1 } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './common/dto';
 
@@ -10,12 +10,16 @@ export class AuthControllerV1 {
   constructor(private readonly authServiceV1: AuthServiceV1) {}
 
   @Post('/localLogin')
+  @Header('Content-Type', 'application/json')
+  @Header('charset', 'utf-8')
   @HttpCode(200)
   localLogin(@Body() loginUserDto: LoginUserDto) {
     return this.authServiceV1.localLogin(loginUserDto);
   }
 
   @Post('/localRegister')
+  @Header('Content-Type', 'application/json')
+  @Header('charset', 'utf-8')
   localRegister(@Body() createUserDto: CreateUserDto) {
     return this.authServiceV1.localRegister(createUserDto);
   }
